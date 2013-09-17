6aaefbbe3c0e9ce26ee42fba456213578f964bfa
Parthas-Menethil <xywhpanda@gmail.com>
2013-09-16 23:10:05 -0400

    Added loop and a user verification prompt

--- main.go
+++ main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, 世界")
+	fmt.Println("Testing Loop...")
+	for i := 0; i < 10; i++{
+		fmt.Println("Loop:", i)
+	}
+	fmt.Println("END Loop")
+	var i string
+	for {
+		fmt.Printf("Please enter the password: ")
+		fmt.Scanf("%s", &i)
+		if i == "WfP9Jtb28xxHwRxt"{
+			break
+		} else {
+			fmt.Println("Invalid Password")
+		}
+	}
+	fmt.Println("Identity Verified")
 }

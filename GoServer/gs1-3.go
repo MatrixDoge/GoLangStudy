46926f023588d4f3ddffe0c3d586106eaf318730
Parthas-Menethil <xywhpanda@gmail.com>
2013-10-16 00:05:25 -0400

    Implement Edit using hard-coded HTML

--- GoServer/gs1-3.go
+++ GoServer/gs1-3.go
@@ -5,6 +5,7 @@
 package main
 import(
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
@@ -25,6 +26,21 @@ func loadPage(title string) (*Page, error) {
 	}
 	return &Page{Title: title, Body: body}, nil
 }
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+}
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>" + 
+	"<form action=\"/save/%s\" method=\"POST\">"+
+	"<textarea name=\"body\">%s</textarea><br />"+
+	"<input type=\"submit\" value=\"save\">"+
+	"</form>", 
+	p.Title, p.Title, p.Body)
+}
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
@@ -32,5 +48,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
